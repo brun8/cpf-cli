1863494a25aa1e2f5b5a9b70a7a6c1dc016154d2
bruno <brunofvteixeira@gmail.com>
2022-11-10 13:15:17 -0300

    refactor flags

--- main.go
+++ main.go
@@ -7,6 +7,25 @@ import (
 )
 
 func main() {
+  opts := parseFlags()
+
+	if opts.region < -1 || opts.region > 9 {
+		fmt.Println("região inválida")
+		flag.Usage()
+		return
+	}
+
+	for i := 0; i < opts.num; i++ {
+		fmt.Println(generator.GenerateCpf(opts.punctuated, opts.region))
+	}
+}
+
+type ProgramFlags struct {
+  num, region int
+  punctuated bool
+}
+
+func parseFlags() ProgramFlags {
 	num := flag.Int("n", 1, "quantidade de dados a serem gerados")
 	region := flag.Int(
 		"r", -1,
@@ -25,13 +44,5 @@ func main() {
 	punctuated := flag.Bool("p", false, "gerar dados com pontuação")
 	flag.Parse()
 
-	if *region < -1 || *region > 9 {
-		fmt.Println("região inválida")
-		flag.Usage()
-		return
-	}
-
-	for i := 0; i < *num; i++ {
-		fmt.Println(generator.GenerateCpf(*punctuated, *region))
-	}
+  return ProgramFlags{*num, *region, *punctuated}
 }
