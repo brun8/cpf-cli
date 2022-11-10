b89cb6f4e3cd99e1d38673165d599fc37533bf4c
bruno <brunofvteixeira@gmail.com>
2022-11-10 12:59:58 -0300

    add tests

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -1,23 +1,73 @@
 package generator
 
-import "testing"
+import (
+	"strconv"
+	"strings"
+	"testing"
+)
 
 func TestGenerateCpfLength(t *testing.T) {
-  unpunctuated := GenerateCpf(false, -1)
-  if len(unpunctuated) != 11 {
-    t.Errorf("wrong cpf length. got=%d, want=%d", len(unpunctuated), 11)
-  }
-  punctuated := GenerateCpf(true, -1)
-  if len(punctuated) != 14 {
-    t.Errorf("wrong cpf length. got=%d, want=%d", len(punctuated), 14)
-  }
+	unpunctuated := GenerateCpf(false, -1)
+	if len(unpunctuated) != 11 {
+		t.Errorf("wrong cpf length. got=%d, want=%d", len(unpunctuated), 11)
+	}
+
+	punctuated := GenerateCpf(true, -1)
+	if len(punctuated) != 14 {
+		t.Errorf("wrong cpf length. got=%d, want=%d", len(punctuated), 14)
+	}
 }
 
 func TestGenerateWithRegion(t *testing.T) {
-  cpf := GenerateCpf(false, 1)
+	cpf := GenerateCpf(false, 1)
 
-  digit := cpf[8]
-  if digit != '1' {
-    t.Errorf("invalid cpf region. got=%c, want=%c", digit, '1')
-  }
+	digit := cpf[8]
+	if digit != '1' {
+		t.Errorf("invalid cpf region. got=%c, want=%c", digit, '1')
+	}
+
+	validateCpf(t, cpf)
+}
+
+func TestCpfIsValid(t *testing.T) {
+	unpunctuated := GenerateCpf(false, -1)
+  punctuated := GenerateCpf(true, -1)
+
+	validateCpf(t, unpunctuated)
+	validateCpf(t, punctuated)
+}
+
+func validateCpf(t *testing.T, cpf string) {
+  cpf = normalizeCpf(cpf)
+
+	var sum1, sum2 int
+	for i := 0; i < 10; i++ {
+		num, err := strconv.Atoi(string(cpf[i]))
+		if err != nil {
+      t.Error("invalid characters on cpf")
+      return
+		}
+
+		// pula o ultimo digito pro calculo do primeiro digito verificador
+		if i != 9 {
+			sum1 += (10 - i) * num
+		}
+
+		// pula o primeiro digito pro calculo do segundo digito verificador
+		if i != 0 {
+			sum2 += (10 - (i - 1)) * num
+		}
+	}
+
+	v1 := generateDigit(sum1)
+	v2 := generateDigit(sum2)
+
+	if strconv.Itoa(v1) != string(cpf[9]) || strconv.Itoa(v2) != string(cpf[10]) {
+    t.Error("invalid cpf")
+	}
+}
+
+func normalizeCpf(cpf string) string {
+	cpf = strings.ReplaceAll(cpf, ".", "")
+	return strings.ReplaceAll(cpf, "-", "")
 }
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"cpf/generator"
 	"flag"
 	"fmt"
-  "cpf/generator"
 )
 
 func main() {
 	num := flag.Int("n", 1, "quantidade de dados a serem gerados")
-  region := flag.Int(
-    "r", -1, 
-    `região emissora do cpf
+	region := flag.Int(
+		"r", -1,
+		`região emissora do cpf
 1 – DF, GO, MS, MT e TO
 2 – AC, AM, AP, PA, RO e RR
 3 – CE, MA e PI
@@ -21,19 +21,17 @@ func main() {
 8 – SP
 9 – PR e SC
 0 – RS`,
-  )
+	)
 	punctuated := flag.Bool("p", false, "gerar dados com pontuação")
 	flag.Parse()
 
-  if *region < -1 || *region > 9 {
-    fmt.Println("região inválida")
-    flag.Usage()
-    return
-  }
+	if *region < -1 || *region > 9 {
+		fmt.Println("região inválida")
+		flag.Usage()
+		return
+	}
 
 	for i := 0; i < *num; i++ {
 		fmt.Println(generator.GenerateCpf(*punctuated, *region))
 	}
 }
-
-
